4a1fee0a221b6af26220d1c2a42bca3d5e8f71ba
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-18 23:23:16 +0700

    fix: minor style

--- resources/views/components/aside-carousel.blade.php
+++ resources/views/components/aside-carousel.blade.php
@@ -1,16 +1,20 @@
-<div id="asideCarousel" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        @foreach ($slides as $index => $slide)
-            <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
-                <img src="{{ $slide['image'] }}" class="d-block w-100" alt="Slide {{ $index + 1 }}">
-            </div>
-        @endforeach
+<div>
+    <h2 class="fs-6 fw-normal d-inline-block border-bottom border-success border-2 pb-1">SUARA WARGA</h2>
+    <div id="asideCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            @foreach ($slides as $index => $slide)
+                <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
+                    <img src="{{ $slide['image'] }}" class="d-block w-100" alt="Slide {{ $index + 1 }}">
+                </div>
+            @endforeach
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#asideCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#asideCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
     </div>
 
-    <button class="carousel-control-prev" type="button" data-bs-target="#asideCarousel" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#asideCarousel" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    </button>
 </div>
--- resources/views/components/video-embed.blade.php
+++ resources/views/components/video-embed.blade.php
@@ -1,4 +1,7 @@
-<iframe width="306" height="175" class="rounded shadow" src="https://www.youtube.com/embed/cTwbVJkLLsc?si=IYe3uBfTkaPtWw8L"
-    title="YouTube video player" frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-</iframe>
+<div>
+    <h2 class="fs-6 fw-normal d-inline-block border-bottom border-success border-2 pb-1">SUARA WARGA</h2>
+    <iframe width="306" height="175" class="rounded shadow"
+        src="https://www.youtube.com/embed/cTwbVJkLLsc?si=IYe3uBfTkaPtWw8L" title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+    </iframe>
+</div>
