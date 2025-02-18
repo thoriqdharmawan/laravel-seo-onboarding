5b13f0554b8e355d143d1445186a2da1959f921c
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-18 16:44:10 +0700

    feat: leatest news

--- resources/views/components/latest-info.blade.php
+++ resources/views/components/latest-info.blade.php
@@ -1,9 +1,14 @@
 <div class="row justify-content-start my-5">
     <div class="col-8">
         <h2 class="fs-6">INFO TERKINI</h2>
-        <div>
-            <h3 class="fs-5">Seleksi Calon Anggota CPNS</h3>
-            <date>Kamis 23/01/2025 18:00 WIB</date>
+        <div class="card text-bg-dark border-0 rounded-lg">
+            <img src="https://placehold.co/110x60" class="card-img" alt="thumbnail">
+            <div class="card-img-overlay d-flex align-items-end bg-black bg-opacity-25">
+                <div>
+                    <time class="card-text" datetime="2025-01-23"><small>Kamis 23/01/2025 18:00 WIB</small></time>
+                    <h3 class="card-title text-warning">Seleksi Calon Anggota CPNS</h3>
+                </div>
+            </div>
         </div>
     </div>
     <div class="col-4">
