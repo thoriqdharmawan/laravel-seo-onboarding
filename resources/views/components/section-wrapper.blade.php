2ddbb7f7d10a7ba60a0b42978428f3fe0784c8e7
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-18 22:50:20 +0700

    fix: minor style

--- resources/views/components/section-wrapper.blade.php
+++ resources/views/components/section-wrapper.blade.php
@@ -1,6 +1,6 @@
 <section>
-    <div class="w-full border-bottom border-danger border-2 m-0">
-        <div class="bg-danger rounded-top-3 d-inline-block px-2 py-1">
+    <div class="w-full border-bottom border-success border-2 m-0">
+        <div class="bg-success rounded-top-3 d-inline-block px-2 py-1">
             <h2 class="fs-6 text-white fw-light m-0">{{ $title }}</h2>
         </div>
     </div>
