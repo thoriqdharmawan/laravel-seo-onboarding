252f0eaa2bfb5115ac875a6e74e896151939c8ef
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-18 23:15:20 +0700

    feat: news items

--- resources/views/components/news-item.blade.php
+++ resources/views/components/news-item.blade.php
@@ -1,7 +1,7 @@
 <div class="col-4">
-    <div class="card border-0 shadow">
-        <img src="https://placehold.co/160x110" class="card-img-top px-3 pt-3 rounded-lg" alt="news thumbnail">
-        <div class="card-body">
+    <div class="card border-0 shadow p-3">
+        <img src="https://placehold.co/160x110" class="card-img rounded-lg" alt="news thumbnail">
+        <div class="card-body pt-3 px-0 pb-0">
             <time class="fs-6" datetime="2025-01-23">Kamis 23/01/2025 18:00 WIB</time>
             <h3 class="fs-5 text-truncate mt-1">Musrenbang Tematik Jaring Aspirasi Wujudkan Lansia</h3>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
