df5231a845fb3715787788a12029c23a05004008
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-18 16:35:28 +0700

    fix: news item horizontal style

--- resources/views/components/news-item-horizontal.blade.php
+++ resources/views/components/news-item-horizontal.blade.php
@@ -1,10 +1,14 @@
-<div class="row justify-content-start mb-4 g-2">
-    <div class="col-3">
-        <img src="https://placehold.co/160x140" class="img-fluid" alt="news thumbnail" />
-    </div>
-    <div class="col-9">
-        <time class="fs-6" datetime="2025-01-23">Kamis 23/01/2025 18:00 WIB</time>
-        <h3 class="fs-5 m-0 text-truncate">Hasil Seleksi Administrasi Calon Anggota Komisaris</h3>
-        <p class="m-0">Sumber: OPD</p>
+<div class="card mb-3 w-full border-0">
+    <div class="row g-0">
+        <div class="col-md-3">
+            <img src="https://placehold.co/160x140" class="img-fluid rounded" alt="thumbnail">
+        </div>
+        <div class="col-md-9">
+            <div class="card-body py-0">
+                <time class="card-text" datetime="2025-01-23"><small>Kamis 23/01/2025 18:00 WIB</small></time>
+                <h3 class="fs-5 m-0 text-truncate">Hasil Seleksi Administrasi Calon Anggota Komisaris</h3>
+                <p class="m-0">Sumber: OPD</p>
+            </div>
+        </div>
     </div>
 </div>
