861576feb09119991715c845c1348bd35cc5faab
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-18 18:27:43 +0700

    feat: add a new twibon action

--- resources/views/components/twibon.blade.php
+++ resources/views/components/twibon.blade.php
@@ -1,13 +1,15 @@
 <div class="card mb-3 border-0 shadow">
     <div class="card-body p-0">
-        <h5 class="card-title text-center border-bottom p-3 text-success-emphasis m-0">Buat Twibon</h5>
-        <p class="m-0 text-center p-2 text-success-emphasis fw-semibold">HUT ke-53 Korpri Kota Yogyakarta</p>
+        <h5 class="card-title text-center border-bottom p-3 text-success m-0">Buat Twibon</h5>
+        <p class="m-0 text-center p-2 text-success fw-semibold">HUT ke-53 Korpri Kota Yogyakarta</p>
     </div>
     <img src="https://placehold.co/120x120" class="card-img rounded-0" alt="twibon">
-    <div class="card-body">
-        <h5 class="card-title p-3 m-0">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-            This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    <div class="card-body py-4 d-flex justify-content-center align-items-center gap-2 flex-column">
+        <div class="d-flex gap-2">
+            <button type="button" class="btn btn-primary bg-black border-0 px-4">Unggah foto</button>
+            <button type="button" class="btn btn-primary bg-black border-0 px-4">Lihat</button>
+        </div>
+
+        <button type="button" class="btn text-success">Lihat twibbon lainnya &gt;</button>
     </div>
 </div>
