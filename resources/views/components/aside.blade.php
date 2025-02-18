fcfe4a28ca507fb7fb04a206643eff003ab93602
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-18 17:53:42 +0700

    feat: add twibon component

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -1,4 +1,4 @@
-<aside class="my-5">
-    <input type="search" class="form-control" aria-describedby="search">
-
+<aside class="my-5 d-flex flex-column gap-4">
+    <input type="search" class="form-control border-0 bg-body-secondary" placeholder="Cari berita apa?" aria-describedby="search">
+    <x-twibon></x-twibon>
 </aside>
---
+++ resources/views/components/twibon.blade.php
@@ -0,0 +1,13 @@
+<div class="card mb-3 border-0 shadow">
+    <div class="card-body p-0">
+        <h5 class="card-title text-center border-bottom p-3 text-success-emphasis m-0">Buat Twibon</h5>
+        <p class="m-0 text-center p-2 text-success-emphasis fw-semibold">HUT ke-53 Korpri Kota Yogyakarta</p>
+    </div>
+    <img src="https://placehold.co/120x120" class="card-img rounded-0" alt="twibon">
+    <div class="card-body">
+        <h5 class="card-title p-3 m-0">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+            This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+</div>
