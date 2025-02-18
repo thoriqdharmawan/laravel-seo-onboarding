dca701d0dda3f6dfd8d01973fb2dd7b873c2d368
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-18 17:39:57 +0700

    feat: slicing footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,21 @@
-<footer class="bg-dark text-white text-center py-3">
-    <p>&copy; 2025 Pemerintah Kota Yogyakarta</p>
+<footer class="bg-dark text-white text-center mt-5">
+    <div class="container">
+        <div class="d-flex flex-column gap-4 py-4 border-bottom border-white">
+            <p class="m-0">Yogyakarta, kota modern yang tertata rapi, menjadi tempat hunian yang nyaman dengan
+                masyarakat yang berkebudayaan dan dengan pemerintahan yang berorientasi pada pelayanan publik.</p>
+
+            <div>
+                <div class="d-flex gap-2 justify-content-center">
+                    <p class="m-0">Pengunjung saat ini : 23</p>
+                    <p class="m-0">Hari ini : 1.359</p>
+                    <p class="m-0">Bulan ini : 224.651</p>
+                    <p class="m-0">Tahun ini : 752.458</p>
+                    <p class="m-0">Total : 32.512.368</p>
+                </div>
+                <p class="m-0">Penerimaan aduan insiden siber melalui email: csirt@jogjakota.go.id</p>
+                <p class="m-0">Kebijakan Privasi Website Pemerintah Kota Yogyakarta</p>
+            </div>
+        </div>
+        <p class="py-3 m-0">&copy; 2025 Pemerintah Kota Yogyakarta</p>
+    </div>
 </footer>
