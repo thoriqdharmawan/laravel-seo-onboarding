8345ecfbfadcbb0aaad0d2eea20a997150d9fe1a
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-18 16:50:11 +0700

    feat: add a opd news section

---
+++ resources/views/components/opd-news.blade.php
@@ -0,0 +1,8 @@
+<x-section-wrapper>
+    <x-slot:title>BERITA OPD</x-slot>
+    <div class="row g-4">
+        <x-news-item></x-news-item>
+        <x-news-item></x-news-item>
+        <x-news-item></x-news-item>
+    </div>
+</x-section-wrapper>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
     <x-latest-info></x-latest-info>
     <x-headlines></x-headlines>
+    <x-opd-news></x-opd-news>
 </x-layout>
